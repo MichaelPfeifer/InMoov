0d26125bb4867507e0271b1095e063c5aa5b7e78
Michael Pfeifer <pfeifermic@gmail.com>
2016-09-23 11:36:32 +0200

    Neuer ServoConfigController hinzugefügt

---
+++ InMoovV1.0/InMoovV1.0/ServoConfigController.swift
@@ -0,0 +1,18 @@
+//
+//  ServoConfigController.swift
+//  InMoovV1.0
+//
+//  Created by Michael Pfeifer on 23.09.16.
+//  Copyright © 2016 Michael Pfeifer. All rights reserved.
+//
+
+import Cocoa
+
+class ServoConfigController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
