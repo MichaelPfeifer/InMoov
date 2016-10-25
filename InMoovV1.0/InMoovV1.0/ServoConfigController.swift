7640adba23d29458f1ca25ef13a27be6900df8fb
Michael Pfeifer <pfeifermic@gmail.com>
2016-10-25 15:07:39 +0200

    Entfernen von JSON

--- InMoovV1.0/InMoovV1.0/ServoConfigController.swift
+++ InMoovV1.0/InMoovV1.0/ServoConfigController.swift
@@ -9,16 +9,69 @@
 import Cocoa
 
 
-class ServoConfigController: NSViewController {
+class ServoConfigController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
     
-    let mgdContext = (NSApplication.shared().delegate as! AppDelegate).managedObjectContext
+    @IBOutlet weak var tableView: NSTableView!
     
-    var servoName: [ServoName]! = [ServoName]()
+    @IBOutlet weak var servoName: NSTextField!
+    @IBOutlet weak var busNummer: NSTextField!
+    @IBOutlet weak var servoMin: NSTextField!
+    @IBOutlet weak var servoMax: NSTextField!
+    @IBOutlet weak var servoHome: NSTextField!
+    @IBOutlet weak var beSchreibung: NSTextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+        }
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return names.count
     }
-  
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        if let column = tableColumn {
+            if let cellView = tableView.make(withIdentifier: column.identifier, owner: nil) as? NSTableCellView {
+                
+                
+                if (column.identifier == "servoname") {
+                    let name = names[row]
+                    cellView.textField?.stringValue = name
+                    return cellView
+                }
+                else if (column.identifier == "busnummer") {
+                    let number = numbers[row]
+                    cellView.textField?.stringValue = number
+                    return cellView
+                }
+                
+                return cellView
+            }
+        }
+        return nil
+    }
+    override var representedObject: Any? {
+        didSet {
+            // Update the view, if already loaded.
+        }
+    }
+    func tableViewSelectionDidChange(_ notification: Notification) {
+        print(tableView.selectedRow)
+        let servo = names[tableView.selectedRow]
+        let number = numbers[tableView.selectedRow]
+        let servomin = servomins[tableView.selectedRow]
+        let servomax = servomaxs[tableView.selectedRow]
+        let servohome = servohomes[tableView.selectedRow]
+        let beschreibung = beschreibungs[tableView.selectedRow]
+        servoName.stringValue = servo
+        busNummer.stringValue = number
+        servoMin.stringValue = servomin
+        servoMax.stringValue = servomax
+        servoHome.stringValue = servohome
+        beSchreibung.stringValue = beschreibung
+    }
     
 }
+
+    
+
